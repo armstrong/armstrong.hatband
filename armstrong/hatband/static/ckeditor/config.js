bf2a419821ebbfe913cbbe710a88b1e7b56e4701
Dave McLain <dmclain@texastribune.org>
2011-06-29 18:09:11 -0500

    make ckeditor work

---
+++ armstrong/hatband/static/ckeditor/config.js
@@ -0,0 +1,12 @@
+﻿CKEDITOR.editorConfig = function( config )
+{
+	config.toolbar = 'MyToolbar';
+ 
+	config.toolbar_MyToolbar =
+	[
+		{ name: 'clipboard', items : [ 'PasteFromWord' ] },
+		{ name: 'styles', items : [ 'Format' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+		{ name: 'links', items : [ 'Link','Unlink'] },
+	];
+};
--- armstrong/hatband/widgets/__init__.py
+++ armstrong/hatband/widgets/__init__.py
@@ -2,3 +2,4 @@ from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
 from armstrong.hatband.widgets.base import RichTextWidget
+from armstrong.hatband.widgets.ckeditor import CKEditorWidget
