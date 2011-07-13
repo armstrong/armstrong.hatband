a6d983371eba9b64333a65ec5f6eca38e66e59fc
Travis Swicegood <development@domain51.com>
2011-07-13 17:42:30 -0500

    add in a custom sites module to override Django's built in one

--- armstrong/hatband/__init__.py
+++ armstrong/hatband/__init__.py
@@ -1,4 +1,52 @@
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
+# Make this a drop-in replacement for Django's built-in Admin
+from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME
+from django.contrib.admin.options import HORIZONTAL, VERTICAL
+
+# Below are overrides that we provide that are Hatband specific
 from .options import ModelAdmin, StackedInline, TabularInline
+from armstrong.hatband.sites import AdminSite, site
+
+
+# This is copied directly from Django as there is no way to specify what `site`
+# object you want to use inside `autodiscover`.  This is important for Hatband,
+# as all of Armstrong will eventually use `from armstrong import hatband as
+# admin` and us Hatband's `site` value.
+#
+# TODO: keep this in sync with django.contrib.admin.autodiscover (ongoing)
+def autodiscover():
+    """
+    Auto-discover INSTALLED_APPS admin.py modules and fail silently when
+    not present. This forces an import on them to register any admin bits they
+    may want.
+
+    .. _warning: This is a direct copy and paste from ``django.contrib.admin``
+                 becuase they do not support providing a different ``site``
+                 than their own.
+    """
+
+    import copy
+    from django.conf import settings
+    from django.utils.importlib import import_module
+    from django.utils.module_loading import module_has_submodule
+
+    for app in settings.INSTALLED_APPS:
+        mod = import_module(app)
+        # Attempt to import the app's admin module.
+        try:
+            before_import_registry = copy.copy(site._registry)
+            import_module('%s.admin' % app)
+        except:
+            # Reset the model registry to the state before the last import as
+            # this import will have to reoccur on the next request and this
+            # could raise NotRegistered and AlreadyRegistered exceptions
+            # (see #8245).
+            site._registry = before_import_registry
+
+            # Decide whether to bubble up this error. If the app just
+            # doesn't have an admin module, we can ignore the error
+            # attempting to import it, otherwise we want it to bubble up.
+            if module_has_submodule(mod, 'admin'):
+                raise
---
+++ armstrong/hatband/sites.py
@@ -0,0 +1,37 @@
+from django.contrib.admin.sites import AdminSite as DjangoAdminSite
+from django.contrib.admin.sites import site as django_site
+
+
+class HatbandAndDjangoRegistry(object):
+    def __init__(self, site):
+        super(HatbandAndDjangoRegistry, self).__init__()
+        self._site = site
+        self._registry = {}
+        self.dicts = [self._registry, django_site._registry]
+
+    def items(self):
+        for d in self.dicts:
+            for item in d.items():
+                yield item
+
+    def iteritems(self):
+        return iter(self.items())
+
+    def __contains__(self, k):
+        return k in self._registry or k in django_site._registry
+
+
+class AdminSite(DjangoAdminSite):
+    def __init__(self, *args, **kwargs):
+        super(AdminSite, self).__init__(*args, **kwargs)
+        self._registry = HatbandAndDjangoRegistry(self)
+
+    def get_urls(self):
+        from django.conf.urls.defaults import patterns, url
+
+        return patterns('',
+            # Custom hatband Views here
+        ) + super(AdminSite, self).get_urls()
+
+
+site = AdminSite()
