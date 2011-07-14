31b681cd498019fdf62b31a8c5d0e59b6ca09d28
Travis Swicegood <development@domain51.com>
2011-07-14 17:53:12 -0500

    Drastically simplify
    This is the reason you need to sleep on code.  There is a better way
    than copy-n-pasting the entire autodiscover mechanism.  Instead, we rely
    Django's auto-discover, then merge any of our registered sites into what
    was registered with the default admin.
    
    This is still less than ideal, but such is life with globals.

--- armstrong/hatband/__init__.py
+++ armstrong/hatband/__init__.py
@@ -10,43 +10,15 @@ from .options import ModelAdmin, StackedInline, TabularInline
 from armstrong.hatband.sites import AdminSite, site
 
 
-# This is copied directly from Django as there is no way to specify what `site`
-# object you want to use inside `autodiscover`.  This is important for Hatband,
-# as all of Armstrong will eventually use `from armstrong import hatband as
-# admin` and use Hatband's `site` value.
-#
-# TODO: keep this in sync with django.contrib.admin.autodiscover (ongoing)
 def autodiscover():
     """
-    Auto-discover INSTALLED_APPS admin.py modules and fail silently when
-    not present. This forces an import on them to register any admin bits they
-    may want.
-
-    .. _warning: This is a direct copy and paste from ``django.contrib.admin``
-                 becuase they do not support providing a different ``site``
-                 than their own.
+    TODO: document
     """
+    from django.contrib.admin import autodiscover as django_autodiscover
+    django_autodiscover()
 
-    import copy
-    from django.conf import settings
-    from django.utils.importlib import import_module
-    from django.utils.module_loading import module_has_submodule
-
-    for app in settings.INSTALLED_APPS:
-        mod = import_module(app)
-        # Attempt to import the app's admin module.
-        try:
-            before_import_registry = copy.copy(site._registry)
-            import_module('%s.admin' % app)
-        except:
-            # Reset the model registry to the state before the last import as
-            # this import will have to reoccur on the next request and this
-            # could raise NotRegistered and AlreadyRegistered exceptions
-            # (see #8245).
-            site._registry = before_import_registry
-
-            # Decide whether to bubble up this error. If the app just
-            # doesn't have an admin module, we can ignore the error
-            # attempting to import it, otherwise we want it to bubble up.
-            if module_has_submodule(mod, 'admin'):
-                raise
+    from copy import copy
+    from django.contrib.admin import site as django_site
+    registry = copy(django_site._registry)
+    registry.update(site._registry)
+    site._registry = registry
--- armstrong/hatband/sites.py
+++ armstrong/hatband/sites.py
@@ -2,33 +2,6 @@ from django.contrib.admin.sites import AdminSite as DjangoAdminSite
 from django.contrib.admin.sites import site as django_site
 
 
-class HatbandAndDjangoRegistry(object):
-    def __init__(self, site, default_site=None):
-        if default_site is None:
-            default_site = django_site
-        super(HatbandAndDjangoRegistry, self).__init__()
-        self._site = site
-        self._registry = {}
-        self.dicts = [self._registry, default_site._registry]
-
-    def items(self):
-        for d in self.dicts:
-            for item in d.items():
-                yield item
-
-    def iteritems(self):
-        return iter(self.items())
-
-    def __contains__(self, k):
-        for d in self.dicts:
-            if k in d:
-                return True
-        return False
-
-    def __setitem__(self, k, v):
-        self._registry[k] = v
-
-
 class AdminSite(DjangoAdminSite):
     def get_urls(self):
         from django.conf.urls.defaults import patterns, url
@@ -39,4 +12,3 @@ class AdminSite(DjangoAdminSite):
 
 
 site = AdminSite()
-site._registry = HatbandAndDjangoRegistry(site, default_site=django_site)
