5d3dd475ff7e693532040c28c72d2cef832de6f5
Travis Swicegood <development@domain51.com>
2011-08-03 10:46:00 -0500

    extract this into its proper home

--- armstrong/hatband/options.py
+++ armstrong/hatband/options.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.contrib.admin.options import InlineModelAdmin
 from django.db import models
 from django import forms
-from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
+
 from . import widgets
-from .utils import static_url
 
 RICH_TEXT_DBFIELD_OVERRIDES = {
     models.TextField: {'widget': widgets.RichTextWidget},
@@ -24,37 +23,11 @@ class TabularInline(admin.TabularInline):
     formfield_overrides = RICH_TEXT_DBFIELD_OVERRIDES
 
 
-class GenericKeyWidget(forms.Widget):
-    template = "admin/hatband/widgets/generickey.html"
-
-    class Media:
-        js = (static_url("visualsearch/dependencies.js"),
-              static_url("visualsearch/visualsearch.js"),
-              static_url("generickey.js"),
-             )
-
-        css = {
-            "all": (static_url("visualsearch/visualsearch.css"), ),
-        }
-
-
-    def __init__(self, *args, **kwargs):
-        super(GenericKeyWidget, self).__init__(*args, **kwargs)
-
-    def render(self, name, value, attrs=None):
-        if value is None:
-            value = ''
-        final_attrs = self.build_attrs(attrs, name=name)
-        final_attrs["value"] = value
-        final_attrs["is_templated"] = final_attrs["id"].find("__prefix__") > -1
-        return render_to_string(self.template, final_attrs)
-
-
 class OrderableGenericKeyLookupForm(forms.ModelForm):
     class Meta:
         widgets = {
             "content_type": forms.HiddenInput(),
-            "object_id": GenericKeyWidget(),
+            "object_id": widgets.GenericKeyWidget(),
             "order": forms.HiddenInput(),
         }
 
--- armstrong/hatband/widgets/__init__.py
+++ armstrong/hatband/widgets/__init__.py
@@ -4,3 +4,4 @@ __path__ = extend_path(__path__, __name__)
 from django.contrib.admin.widgets import *
 from .base import RichTextWidget
 from .ckeditor import CKEditorWidget
+from .visualsearch import GenericKeyWidget
