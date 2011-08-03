cd6267502758b4cb81c753186c4127ad460456b8
Travis Swicegood <development@domain51.com>
2011-08-03 10:48:52 -0500

    extract the form out into its own module

--- armstrong/hatband/options.py
+++ armstrong/hatband/options.py
@@ -1,38 +1,24 @@
 from django.contrib import admin
 from django.contrib.admin.options import InlineModelAdmin
 from django.db import models
-from django import forms
 from django.utils.safestring import mark_safe
 
-from . import widgets
-
-RICH_TEXT_DBFIELD_OVERRIDES = {
-    models.TextField: {'widget': widgets.RichTextWidget},
-}
+from . import forms
 
 
 class ModelAdmin(admin.ModelAdmin):
-    formfield_overrides = RICH_TEXT_DBFIELD_OVERRIDES
+    formfield_overrides = forms.RICH_TEXT_DBFIELD_OVERRIDES
 
 
 class StackedInline(admin.StackedInline):
-    formfield_overrides = RICH_TEXT_DBFIELD_OVERRIDES
+    formfield_overrides = forms.RICH_TEXT_DBFIELD_OVERRIDES
 
 
 class TabularInline(admin.TabularInline):
-    formfield_overrides = RICH_TEXT_DBFIELD_OVERRIDES
-
-
-class OrderableGenericKeyLookupForm(forms.ModelForm):
-    class Meta:
-        widgets = {
-            "content_type": forms.HiddenInput(),
-            "object_id": widgets.GenericKeyWidget(),
-            "order": forms.HiddenInput(),
-        }
+    formfield_overrides = forms.RICH_TEXT_DBFIELD_OVERRIDES
 
 
 class GenericKeyInline(InlineModelAdmin):
-    form = OrderableGenericKeyLookupForm
-    formfield_overrides = RICH_TEXT_DBFIELD_OVERRIDES
+    form = forms.OrderableGenericKeyLookupForm
+    formfield_overrides = forms.RICH_TEXT_DBFIELD_OVERRIDES
     template = "admin/edit_inline/generickey.html"
