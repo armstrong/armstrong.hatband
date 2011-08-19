a2341e400046d737e518b1ed631adcdbc6a4a422
Travis Swicegood <development@domain51.com>
2011-08-19 17:09:41 -0500

    allow facet and query lookup URLs to be configured

--- armstrong/hatband/widgets/visualsearch.py
+++ armstrong/hatband/widgets/visualsearch.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.forms import Widget
 from django.template.loader import render_to_string
 
@@ -21,10 +22,15 @@ class GenericKeyWidget(Widget):
 
 
     def __init__(self, object_id_name="object_id",
-                 content_type_name="content_type", *args, **kwargs):
+                 content_type_name="content_type",
+                 facet_url=None,
+                 query_lookup_url=None,
+                 *args, **kwargs):
         super(GenericKeyWidget, self).__init__(*args, **kwargs)
         self.object_id_name = object_id_name
         self.content_type_name = content_type_name
+        self.facet_url = facet_url
+        self.query_lookup_url = query_lookup_url
 
     def render(self, name, value, attrs=None):
         if value is None:
@@ -35,5 +41,9 @@ class GenericKeyWidget(Widget):
             "is_templated": final_attrs["id"].find("__prefix__") > -1,
             "object_id_name": self.object_id_name,
             "content_type_name": self.content_type_name,
+            "facet_url": self.facet_url or reverse("admin:generic_key_facets"),
+            "query_lookup_url": (self.query_lookup_url or
+                    reverse("admin:type_and_model_to_query")),
+
         })
         return render_to_string(self.template, final_attrs)
