a4b8de247681a9817789b2473b48b0992d73bb4a
Dave McLain <dmclain@texastribune.org>
2011-10-20 15:18:45 -0500

    remove unused functions

--- armstrong/hatband/static/hatband/js/backbone-inline-base.js
+++ armstrong/hatband/static/hatband/js/backbone-inline-base.js
@@ -27,17 +27,3 @@ var Item = Backbone.Model.extend({
         input.val(this.attributes[name]);
     }
 });
-
-var ItemList = Backbone.Collection.extend({
-    model: Item
-});
-
-var ListItemView = Backbone.View.extend({
-
-});
-
-var ListView = Backbone.View.extend({
-    render: function() {
-        //_.each(this.list, function(
-    }
-});
