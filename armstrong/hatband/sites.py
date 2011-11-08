130da306ed49e6d395bd6a7d8a5075631f72b3c1
Jon Cotton <jcotton@baycitizen.org>
2011-11-08 14:45:35 -0800

    Handle error cases for VisualSearch calls.

--- armstrong/hatband/sites.py
+++ armstrong/hatband/sites.py
@@ -1,3 +1,5 @@
+import json
+
 from django.conf import settings
 from django.contrib.admin.sites import AdminSite as DjangoAdminSite
 from django.contrib.contenttypes.models import ContentType
@@ -49,13 +51,15 @@ class AdminSite(DjangoAdminSite):
                     name="render_model_preview"
                 )
         ]
-        
+
         urlpatterns = patterns('', *search)
 
         return urlpatterns + super(AdminSite, self).get_urls()
 
     @json_response
     def generic_key_facets(self, request):
+        """Find all available facets/Models for VisualSearch"""
+
         excluded_apps = Q(app_label__in=EXCLUDED_APPS_FROM_FACETS)
         excluded_models = Q()
         for app_label, model in EXCLUDED_MODELS_FROM_FACETS:
@@ -66,19 +70,43 @@ class AdminSite(DjangoAdminSite):
                 .exclude(excluded_apps | excluded_models)
         return dict([(str(a), {"app_label": str(b), "id": str(c)}) for a, b, c in content_types])
 
-    @json_response
     def type_and_model_to_query(self, request):
-        # TODO: handle error 404 error state
-        # TODO: handle invalid request error state
-        type = ContentType.objects.get(pk=request.GET["content_type_id"])
-        model = type.model_class().objects.get(pk=request.GET["object_id"])
-        return {"query": '%s: "%d: %s"' % (type.model, model.pk, str(model))}
+        """
+        Return JSON for an individual Model instance
+
+        If the required parameters are wrong, return 400 Bad Request
+        If the parameters are correct but there is no data, return empty JSON
+
+        """
+        try:
+            ctype = ContentType.objects.get(pk=request.GET.get("content_type_id"))
+        except ContentType.DoesNotExist:
+            return HttpResponseBadRequest()
+
+        try:
+            model = ctype.model_class()
+            result = model.objects.get(pk=request.GET["object_id"])
+        except KeyError:
+            return HttpResponseBadRequest()
+        except model.DoesNotExist:
+            data = ""
+        else:
+            data = '%s: "%d: %s"' % (ctype.model, result.pk, result)
+
+        return HttpResponse(json.dumps({"query": data}))
 
     def generic_key_modelsearch(self, request, app_label, model_name):
-        # TODO: deal with missing/bad requests
+        """
+        Find instances for the requested model and return them as JSON.
         # TODO: add test coverage for this
-        content_type = ContentType.objects.get(app_label=app_label, model=model_name)
-        model  = content_type.model_class()
+
+        """
+        try:
+            ctype = ContentType.objects.get(app_label=app_label, model=model_name)
+        except ContentType.DoesNotExist:
+            return HttpResponseBadRequest()
+
+        model = ctype.model_class()
         model_admin = self._registry[model].__class__(model, self)
         model_admin.change_list_template = "admin/hatband/change_list.json"
         return model_admin.changelist_view(request)
