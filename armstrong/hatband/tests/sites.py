2148cca07decb79ffd1c9910be025581d903ccc5
Travis Swicegood <development@domain51.com>
2011-12-09 15:06:59 -0600

    Fix issue where type_and_model_to_query just wouldn't work

--- armstrong/hatband/tests/sites.py
+++ armstrong/hatband/tests/sites.py
@@ -1,3 +1,5 @@
+import json
+
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
@@ -13,6 +15,14 @@ __all__ = [
     "TypeAndModelToQueryViewTestCase",
     "RenderModelPreviewTestCase",
 ]
+
+
+def staff_login(func):
+    def inner(self, *args, **kwargs):
+        self.client.login(username=self.staff.username, password=PASSWORD)
+        return func(self, *args, **kwargs)
+    return inner
+
 
 class HatbandViewTestCase(TestCase):
     def setUp(self):
@@ -79,9 +89,51 @@ class TypeAndModelToQueryViewTestCase(HatbandViewTestCase):
                 "content_type_id": content_type.pk,
                 "object_id": obj.pk})
 
+    @staff_login
+    def test_query_is_inside_response(self):
+        response = self.get_response()
+        data = json.loads(response.content)
+        self.assertTrue("query" in data)
+
+    @staff_login
+    def test_bad_response_on_unknown_content_type(self):
+        response = self.client.get(self.url, {"content_type_id": -1})
+        self.assertEqual(400, response.status_code)
+
+    @staff_login
+    def test_bad_response_on_no_object_id(self):
+        content_type = ContentType.objects.all()[0]
+        response = self.client.get(self.url,
+                {"content_type_id": content_type.pk})
+        self.assertEqual(400, response.status_code)
+
+    @staff_login
+    def test_returns_empty_query_on_unknown_object_id(self):
+        content_type = ContentType.objects.all()[0]
+        response = self.client.get(self.url,
+                {"content_type_id": content_type.pk, "object_id": -1})
+        self.assertEqual(200, response.status_code)
+
+        data = json.loads(response.content)
+        self.assertTrue("query" in data)
+        self.assertEqual("", data["query"])
+
+    @staff_login
+    def test_returns_string_suitable_for_display_in_gfk_widget(self):
+        content_type = ContentType.objects.all()[0]
+        obj = content_type.model_class().objects.all()[0]
+        response = self.client.get(self.url, {
+                "content_type_id": content_type.pk,
+                "object_id": obj.pk})
+        data = json.loads(response.content)
+        query = data["query"]
+        self.assertEqual('%s: "%d: %s"' % (content_type.model, obj.pk, obj, ),
+                         query)
+
 
 class RenderModelPreviewTestCase(HatbandViewTestCase):
     view_name = "admin:render_model_preview"
+
     def get_response(self):
         cat = TestCategory.objects.create(name="cat")
         TestArticle.objects.create(text="Hooray", category=cat)
