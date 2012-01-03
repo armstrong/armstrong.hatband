50af22a689eeebbaf3798b14e66f1a1f01df37d7
Noah Seger <nosamanuel@gmail.com>
2012-01-03 16:26:26 -0600

    Remove additional case of nondeterministic content type

--- armstrong/hatband/tests/sites.py
+++ armstrong/hatband/tests/sites.py
@@ -160,7 +160,7 @@ class TypeAndModelToQueryViewTestCase(HatbandViewTestCase):
 
     @staff_login
     def test_returns_string_suitable_for_display_in_gfk_widget(self):
-        content_type = ContentType.objects.all()[0]
+        content_type = ContentType.objects.get_for_model(User)
         obj = content_type.model_class().objects.all()[0]
         response = self.client.get(self.url, {
                 "content_type_id": content_type.pk,
