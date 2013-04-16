df6804dc2f5ea3e03b43199050b0499b07638afb
Sergio Soria <ssoria@gmail.com>
2013-04-16 12:47:50 -0700

    Adding support for the storages-aware version of the static tag.

--- armstrong/hatband/templatetags/shims.py
+++ armstrong/hatband/templatetags/shims.py
@@ -1,14 +1,23 @@
 from django import VERSION
-from django.conf import settings
-from django.template import Library
+from django.template import Library, Node
 
 register = Library()
 
 
-@register.simple_tag
-def admin_media_prefix():
+class AdminMediaPrefixShimNode(Node):
+    def __init__(self, admin_media_prefix):
+        self.admin_media_prefix = admin_media_prefix
+
+    def render(self, context):
+        return self.admin_media_prefix
+
+
+def do_admin_media_prefix(parser, token):
     if VERSION[0] == 1 and VERSION[1] < 4:
         from django.contrib.admin.templatetags.adminmedia import admin_media_prefix
-        return admin_media_prefix()
+        return AdminMediaPrefixShimNode(admin_media_prefix())
     else:
-        return settings.STATIC_URL + 'admin/'
+        from django.contrib.staticfiles.templatetags.staticfiles import do_static
+        from django.template.base import Token, TOKEN_TEXT
+        return do_static(parser, Token(TOKEN_TEXT, "static 'admin/'"))
+register.tag('admin_media_prefix_shim', do_admin_media_prefix)
