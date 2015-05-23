d782e73cf3c63c79a3b5763d2482ef7d73a73e2e
Jon Cotton <jc@finaltransfer.net>
2015-05-22 18:54:08 -0700

    update VisualSearch to 0.5.0 (and its dependencies); update Invoke task
    See http://documentcloud.github.io/visualsearch/#changelog
    Invoke task now ensures there is no cruft in the destination.

--- tasks.py
+++ tasks.py
@@ -4,5 +4,6 @@ from armstrong.dev.tasks import *
 
 @task
 def update_visualsearch():
+    run("rm -rf ./armstrong/hatband/static/visualsearch/* ./armstrong/hatband/static/images/embed/icons/*")
     run("cp -R ./vendor/visualsearch/build-min/* ./armstrong/hatband/static/visualsearch/")
     run("cp ./vendor/visualsearch/lib/images/embed/icons/* ./armstrong/hatband/static/images/embed/icons/")
